99a4c8a960ed7fc27a3d9c6de79e90b0816e1a87
项连 <root@dsnbc.com>
2025-11-19 15:13:51 +0800

    bubild

--- xmusicAdmin/src/assets/common/apis.js
+++
@@ -1,7 +0,0 @@
-const _root = `${process.env.NODE_ENV == 'development' ? 'http://192.168.2.4:8080/' : '/'}`;
-export const apis = {
-    //调用接口:后端地址 //接口功能
-    get_all_category: `${_root}no-auth/category/all-categories`,//获取所有分类
-    get_all_goods: `${_root}no-auth/product/list`,//获取所有商品
-    get_goods_detail: `${_root}no-auth/product/detail/`,//获取商品详情
-};
